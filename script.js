36d8e6529e0d0426d54a473554353eda6d24d703
Del1rius <timothy.aidan.barry@gmail.com>
2023-06-01 15:54:15 +0200

    fnished most of it

---
+++ script.js
@@ -0,0 +1,72 @@
+// let array1 = document.getElementById("input")
+let array = [];
+
+function add () {
+    let valueBe = document.getElementById("input").value
+    array.push(valueBe)
+    valueBe = document.getElementById("dis").innerHTML = array;
+}
+
+let people = [
+  {
+    name: "Timothy",
+    surname: "Barry",
+    age: 20,
+  },
+  {
+    name: "Sidwell",
+    surname: "Batyi",
+    age: 18,
+  },
+  {
+
+    name: "Lyle",
+    surname: "Brown",
+    age: 23,
+  },
+  {
+
+    name: "Xola",
+    surname: "Bangaza",
+    age: 18,
+  },
+  {
+
+    name: "Olwethu",
+    surname: "Bhiya",
+    age: 18,
+  },
+  {
+
+    name: "Zulaigah",
+    surname: "Benjamin",
+    age: 20,
+  },
+  {
+
+    name: "Marcel",
+    surname: "Brown",
+    age: 19,
+  },
+  {
+
+    name: "Ridhaa",
+    surname: "Gabier",
+    age: 22,
+  }
+];
+
+
+let myPeople = document.getElementById("objectsPeople");
+people.forEach((per) => {
+  myPeople.innerHTML += `
+  <table class="table table-dark text-white text-center m-0 p-0 border-white">
+        <tr class="" style="width: 33%">
+            <td class="fs-4 text-center border-2" style="width: 33%">${per.name}</td>
+            <td class="fs-4 text-center border-2" style="width: 33%">${per.surname}</td>
+            <td class="fs-4 text-center border-2" style="width: 33%">${per.age}</td>
+        </tr>
+    </table>
+    `;
+});
+
